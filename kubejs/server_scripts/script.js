608590a0b0d63253654ab890f8c58b99203e7033
Alexis Rabiller <admin@MacBook-Pro-de-Alexis.local>
2021-11-24 22:42:26 +0100

    first test recipe

--- kubejs/server_scripts/script.js
+++ kubejs/server_scripts/script.js
@@ -5,16 +5,13 @@ settings.logRemovedRecipes = true
 settings.logSkippedRecipes = false
 settings.logErroringRecipes = true
 
-console.info('Hello, World! (You will see this line every time server resources reload)')
-
 onEvent('recipes', event => {
-	// Change recipes here
+	
+	event.remove({input: '#forge:ores/copper'})
+
 })
 
 onEvent('item.tags', event => {
-	// Get the #forge:cobblestone tag collection and add Diamond Ore to it
-	// event.get('forge:cobblestone').add('minecraft:diamond_ore')
-
-	// Get the #forge:cobblestone tag collection and remove Mossy Cobblestone from it
-	// event.get('forge:cobblestone').remove('minecraft:mossy_cobblestone')
+	
 })
+
