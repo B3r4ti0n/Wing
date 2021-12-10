32d7548579d953aad61a79acd68faf99b7485b46
Alexis Rabiller <admin@MacBook-Pro-de-Alexis.local>
2021-12-10 22:53:35 +0100

    update modpack

--- kubejs/startup_scripts/script.js
+++ kubejs/startup_scripts/script.js
@@ -9,5 +9,6 @@ onEvent('item.registry', event => {
 
 onEvent('block.registry', event => {
 	// Register new blocks here
-	// event.create('example_block').material('wood').hardness(1.0).displayName('Example Block')
+	event.create('new_block').material('lantern').hardness(40.0).displayName('NewBlock').texture('../kubejs/assets/kubejs/textures/block/new_block.png').harvestTool('hoe', 4)
 })
+//Caius Hosdius Geta
